6c1024fc5db68b1a278c9d53797df68cfc41036c
VarunThfc <varun.kaundinya13@gmail.com>
2023-04-06 22:09:54 -0500

    Client Changes

--- src/main/java/com/wisc/raft/service/ServerClientConnectionService.java
+++ src/main/java/com/wisc/raft/service/ServerClientConnectionService.java
@@ -24,6 +24,7 @@ public class ServerClientConnectionService extends ServerClientConnectionGrpc.Se
         Client.MetaDataResponse.Builder metaDataResponseBuilder = Client.MetaDataResponse.newBuilder();
         if(reqString.equals("LEADER_CONNECT")){
             if(this.server.getState().getNodeType().equals(Role.LEADER)) {
+                System.out.println("[getLeader] : I am the leader, GREAT !");
                 Optional<Raft.ServerConnect> leaderOpt = this.server.getCluster().stream().filter(serv -> serv.getServerId() != Integer.parseInt(this.server.getState().getNodeId())).findAny();
                 if (leaderOpt.isPresent()) {
                     Raft.ServerConnect serverConnect = leaderOpt.get();
@@ -39,8 +40,11 @@ public class ServerClientConnectionService extends ServerClientConnectionGrpc.Se
                 res.onCompleted();
             }
             else{
+
                 Optional<Raft.ServerConnect> leaderOpt = this.server.getCluster().stream().filter(serv -> serv.getServerId() != Integer.parseInt(this.server.getState().getVotedFor())).findAny();
                 if (leaderOpt.isPresent()) {
+                    System.out.println("[getLeader] : FOLLOWER : " + leaderOpt.get());
+
                     Raft.ServerConnect serverConnect = leaderOpt.get();
                     Client.MetaDataResponse metaDataResponse = metaDataResponseBuilder.setServerId(serverConnect.getServerId()).setPort(serverConnect.getEndpoint().getPort()).setHost(serverConnect.getEndpoint().getHost()).build();
                     res.onNext(metaDataResponse);
