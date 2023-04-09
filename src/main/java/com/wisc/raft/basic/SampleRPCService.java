ce38f31a6d938851aeaf9444ee5985bdac61c08c
VarunThfc <varun.kaundinya13@gmail.com>
2023-04-09 15:21:28 -0500

    sandjd

--- src/main/java/com/wisc/raft/basic/SampleRPCService.java
+++ src/main/java/com/wisc/raft/basic/SampleRPCService.java
@@ -1,15 +1,20 @@
 package com.wisc.raft.basic;
 
+import com.wisc.raft.RaftServer;
 import com.wisc.raft.proto.Sample;
 import com.wisc.raft.proto.SampleServiceGrpc;
 import com.wisc.raft.service.Database;
 import com.wisc.raft.service.SampleDatabase;
 import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.wisc.raft.proto.Client;
 
 import java.util.List;
 
 public class SampleRPCService extends SampleServiceGrpc.SampleServiceImplBase {
+    private static final Logger logger = LoggerFactory.getLogger(RaftServer.class);
+
     private SampleDatabase db;
     static int count = 0;
 
@@ -19,22 +24,22 @@ public class SampleRPCService extends SampleServiceGrpc.SampleServiceImplBase {
 
     @Override
     public void appendEntries(Sample.SampleAppendEntriesRequest sampleAppendEntriesRequest, StreamObserver<Sample.SampleAppendEntriesResponse> res){
-        Sample.SampleLogEntry entries = sampleAppendEntriesRequest.getEntriesList().get(0);
+        List<Sample.SampleLogEntry> entries = sampleAppendEntriesRequest.getEntriesList();
         Sample.SampleAppendEntriesResponse.Builder responseBuilder = Sample.SampleAppendEntriesResponse.newBuilder().setSuccess(false).setTerm(1).setLastMatchTerm(1).setLastMatchIndex(1);
-        int commit = db.commit(entries);
-        Sample.SampleAppendEntriesResponse response;
-        if(commit != -1){
-            response = responseBuilder.setSuccess(true).build();
-            count++;
-            if(count % 50 == 0){
-                System.out.println(count);
+
+        entries.stream().forEach( en -> {
+           int commit = db.commit(en);
+            logger.info(count + " :  " + commit +" : " + System.currentTimeMillis());
+
+            if(commit != -1){
+                count++;
             }
-        }
-        else{
-            response = responseBuilder.build();
-        }
+
+        });
+        Sample.SampleAppendEntriesResponse response = responseBuilder.setSuccess(true).build();
         res.onNext(response);
         res.onCompleted();
+
     }
 
 }
--- src/main/java/com/wisc/raft/basic/SampleServerClient.java
+++ src/main/java/com/wisc/raft/basic/SampleServerClient.java
@@ -35,9 +35,8 @@ public class SampleServerClient {
     }
 
     public static void main (String [] args) {
-        Runnable initiateSubmit = () -> submitObject();
         cluster = new ArrayList<>();
-        for (int i = 2; i < args.length; i++) {
+        for (int i = 2; i < args.length-1; i++) {
             String[] arg = args[i].split("_");
             if (arg[0] == args[0]) continue;
             int serverId = Integer.parseInt(arg[0]);
@@ -48,43 +47,62 @@ public class SampleServerClient {
 
         }
         submitObjectService = Executors.newSingleThreadScheduledExecutor();
-        submitObjects = submitObjectService.scheduleAtFixedRate(initiateSubmit, 5, 1, TimeUnit.SECONDS);
-        submitExecutor = new ThreadPoolExecutor(cluster.size()*10, cluster.size()*50, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+        long time = System.currentTimeMillis();
+        Runnable initiateSubmit = () -> submitObject(args[args.length-1],time);
+
+        submitObjects = submitObjectService.scheduleAtFixedRate(initiateSubmit, 0, 160, TimeUnit.MILLISECONDS);
+        submitExecutor = new ThreadPoolExecutor(cluster.size(), cluster.size(), 60, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
 
     }
 
-    public static void submitObject() {
-        long initTime = System.currentTimeMillis();
-        boolean isRunning = true;
-        while (isRunning){
-            Sample.SampleLogEntry logEntry = Sample.SampleLogEntry.newBuilder().setTerm(key).
-                    setCommand(Sample.SampleCommand.newBuilder().setKey(key).setValue(value).setCommandType("HelloWorld"))
-                    .setIndex(String.valueOf(key)).build();
+    public static void submitObject(String limit, long time) {
 
-            cluster.stream().forEach(cl -> submitExecutor.submit(() -> appendEntries(cl.getEndpoint(), logEntry)));
-            key++;
-            value++;
-            isRunning = (System.currentTimeMillis() - initTime > 950);
+        boolean isRunning = true;
+        while (isRunning) {
+            long t1 = System.currentTimeMillis();
+            List<Sample.SampleLogEntry> alist = new ArrayList<>();
+            while(true) {
+
+                Sample.SampleLogEntry logEntry = Sample.SampleLogEntry.newBuilder().setTerm(key).setCommand(Sample.SampleCommand.newBuilder().setKey(key).setValue(value).setCommandType("HelloWorld")).setIndex(String.valueOf(key)).build();
+                alist.add(logEntry);
+                key++;
+                value++;
+                logger.info(limit);
+                if(t1 - System.currentTimeMillis() > 80 || key == Integer.parseInt(limit)) {
+                    break;
+                }
+            }
+
+            cluster.stream().forEach(cl -> submitExecutor.submit(() -> appendEntries(cl.getEndpoint(), alist)));
+
+            if(key == Integer.parseInt(limit)){
+                logger.info(String.valueOf(time));
+                isRunning = false;
+                submitObjects.cancel(true);
+            }
+
         }
 
     }
 
-    private static void appendEntries(Sample.SampleEndpoint endpoint, Sample.SampleLogEntry logEntry) {
+    private static void appendEntries(Sample.SampleEndpoint endpoint, List<Sample.SampleLogEntry> logEntry) {
         try{
 
         ManagedChannel channel = ManagedChannelBuilder.forAddress(endpoint.getHost(), endpoint.getPort()).usePlaintext().build();
         SampleServiceGrpc.SampleServiceBlockingStub raftServiceBlockingStub = SampleServiceGrpc.newBlockingStub(channel);
-        List<Sample.SampleLogEntry> alist = new ArrayList<>();
-        alist.add(logEntry);
-        Sample.SampleAppendEntriesRequest req = Sample.SampleAppendEntriesRequest.newBuilder().addAllEntries(alist).
+
+        Sample.SampleAppendEntriesRequest req = Sample.SampleAppendEntriesRequest.newBuilder().addAllEntries(logEntry).
                 setCommitIndex(1).setTerm(2).setLastAppendedLogIndex(1).setLastAppendedLogTerm(1).setIndexTracked(1).setLeaderId("1").build();
 
             Sample.SampleAppendEntriesResponse response = raftServiceBlockingStub.appendEntries(req);
+
+            channel.shutdownNow();
 
         }
         catch(Exception e){
             System.out.println("HW " + e);
         }
+
     }
 
 
